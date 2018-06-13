9e5bcb7498b4e2bf69b3dea98d342a01cbd7f91a
ojonczyk <o.jonczyk@gmail.com>
2018-06-14 00:57:37 +0200

    dopasowano do bazy backendu

--- xkom/xkom/spiders/ceneo.py
+++ xkom/xkom/spiders/ceneo.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import datetime
+import json
 from scrapy.selector import Selector
 
 class CeneoSpider(scrapy.Spider):
     name = 'ceneo'
     allowed_domains = ['ceneo.pl']
-    start_urls = [
-        'https://www.ceneo.pl/47629930' ]
+    with open('ceneo_links.json') as json_data:
+        links = json.load(json_data)
+   # print(links)
+    link_dict = {}
+    for data in links:
+        link_dict[data['link']] = data['id']
+    #print(link_dict)
+    #print(link_dict.keys())
+    start_urls = link_dict.keys()
 
     def parse(self, response):
         #response.css('tr').xpath('@data-price').extract()
         
         offers = response.css(".product-offer")
-        item_name = response.xpath('//*[@id="body"]/div[2]/div/div/div[1]/article/table/tr[2]/td/h1/text()').extract()
-        item_image = response.xpath('//*[@class="js_gallery-anchor js_image-preview"]/img/@src').extract()
-        
+
+      #  print(response.request.url)
+
+        best_price = offers[0].xpath('@data-price').extract()
+
         for offer in offers:
-            yield {
-                    'name': item_name,
-                    'shop_name': offer.xpath('@data-shopurl').extract(),
-					'price': offer.xpath('@data-price').extract(),
-					'imageURL': item_image ,
-                    'productUrl': offer.xpath('./td[1]/a/@href').extract(),
-					'description': offer.css('.short-name__txt ::text').extract() 
-                  }
+            if best_price > offer.xpath('@data-price').extract():
+                best_price = offer.xpath('@data-price').extract()
+
+        yield {
+                'product_link_id': self.link_dict[response.request.url],
+				'price': float(best_price[0]),
+				'timestamp': datetime.datetime.now() 
+            }
