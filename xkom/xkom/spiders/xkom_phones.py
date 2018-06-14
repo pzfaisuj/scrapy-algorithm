a3be97cbaf485b75c43ee268e8fb0bc1da29cd86
Mokasyn56 <hubert_b@op.pl>
2018-06-14 09:47:14 +0200

    xkom spider

--- xkom/xkom/spiders/xkom_phones.py
+++ xkom/xkom/spiders/xkom_phones.py
@@ -5,28 +5,27 @@ from scrapy.selector import Selector
 class XkomSpider(scrapy.Spider):
     name = 'xkom'
     allowed_domains = ['x-kom.pl']
-    start_urls = ['https://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html']
+    with open('xkom_links.json') as json_data:
+        links = json.load(json_data)
+   # print(links)
+    link_dict = {}
+    for data in links:
+        link_dict[data['link']] = data['id']
+    #print(link_dict)
+    #print(link_dict.keys())
+    start_urls = link_dict.keys()
 
     def parse(self, response):
-#phones = []
-#results = response.selector.xpath('//div[@class="product-item product-impression"]').extract()
-#for result in results:
-#    item = SklepyItem()
-#    item['name'] = result.xpath('//div[@class="description-wrapper"]/text').extract()
-#    item['price'] = response.xpath('//div[@data-product-price]').extract()
-#    phones.append(item)
-#return phones
-        results = response.css('.product-item')
-        for result in results:
-            str = 'https://www.x-kom.pl'
-            str2 = result.xpath('./div/a/@href').extract()
-            str3 = ''.join(str2)
-            str+=str3
-            yield {
-                    'name': result.xpath('./@data-product-name').extract()[0],
-					'price': result.xpath('./@data-product-price').extract()[0],
-					'imageURL': result.xpath('./a/img/@src').extract()[0],
-					'productId': result.xpath('./@data-product-id').extract()[0],
-                    'productUrl': str,
-					'description': result.xpath('./div/div/ul/@title').extract()[0]
-                  }
+        #response.css('tr').xpath('@data-price').extract()
+        
+        offers = response.css(".product-detail-impression")
+
+      #  print(response.request.url)
+
+        best_price = offers[0].xpath('./@data-product-price').extract()
+
+        yield {
+                'product_link_id': self.link_dict[response.request.url],
+				'price': float(best_price[0]),
+				'timestamp': datetime.datetime.now() 
+            }
