38b169fbe7b0ad78796d2c6668ee96c2b6dcd628
tomek <tomtomm77@gmail.com>
2018-06-22 12:29:18 +0200

    komputronik updated

--- xkom/xkom/spiders/komputronik.py
+++ xkom/xkom/spiders/komputronik.py
@@ -1,5 +1,6 @@
 import scrapy
-
+import datetime
+import json
 
 class XkomSpider(scrapy.Spider):
 
@@ -7,10 +8,21 @@ class XkomSpider(scrapy.Spider):
 
     name = 'komputronik'
     allowed_domains = ['komputronik.pl']
-    start_urls = []
-    for i in range(1, 16):
-        start_urls.append('https://www.komputronik.pl/category/1596/smartfony.html?p=' + str(i))
-    # start_urls = ['https://www.komputronik.pl/category/1596/smartfony.html']
+
+    with open('../../komputronik_links.json') as json_data:
+        links = json.load(json_data)
+        # print(links)
+    link_dict = {}
+    for data in links:
+        link_dict[data['link']] = data['id']
+
+    start_urls = link_dict.keys()
+
+    # start_urls = []
+    # for i in range(1, 16):
+    #     start_urls.append('https://www.komputronik.pl/category/1596/smartfony.html?p=' + str(i))
+    # # start_urls = ['https://www.komputronik.pl/category/1596/smartfony.html']
+
     cls = "product-entry2-wrap"
 
     def strip(self, string):
@@ -28,30 +40,14 @@ class XkomSpider(scrapy.Spider):
 
     def parse(self, response):
 
+        price = response.css('html body.fgm-fixed ktr-site div#content-wrapper ktr-product div ktr-transclude div#p-inner.pgrid-container div#p-inner-right.col-xs-30.col-lg-8.pull-right div.row div.col-md-14.col-lg-30 section#p-inner-prices div.prices span.price span.proper')
+        result = self.strip(response.css("span.proper:nth-child(1)")[0].select("text()").extract()[0])
 
-        product_list = response.css('.product-entry2')
-        for product in product_list:
-
-            try:
-                pr_id = product.xpath('.//div[@class="pe2-codes"]/text()').extract()
-            except IndexError:
-                pr_id = "Not available"
-
-            try:
-                descr = product.xpath('.//div[@class="inline-features"]/span/text()').extract()[0]
-            except IndexError:
-                descr = str("Not available")
-
-
-            yield {
-                'name': self.strip(product.xpath('.//div[@class="pe2-head"]/a/text()').extract()[0].splitlines()[1]),
-                'price': self.strip(product.xpath('.//div[@class="ps4-price"]/span/text()').extract()[0]),
-                'imageURL': product.xpath('.//div[@class="pe2-head"]/a/@href').extract(),
-                'productId': self.strip(pr_id),
-                'productUrl': str(product.xpath('.//div[@class="pe2-img"]/a/img/@data-src').extract())[4:],
-                'description': self.strip(descr)
-
-            }
+        yield {
+            'product_link_id': self.link_dict[response.request.url],
+            'price': float(result.replace(" ", "")),
+            'timestamp': datetime.datetime.now()
+        }
 
 
 
