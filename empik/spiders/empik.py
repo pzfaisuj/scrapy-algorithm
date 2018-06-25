973a5b71526d99572db67d23d387bfbf2dd65bbc
arecki2 <malina474@gmail.com>
2018-06-25 06:16:14 +0200

    Update empik.py

--- empik/spiders/empik.py
+++ empik/spiders/empik.py
@@ -31,15 +31,14 @@ class EmpikSpider(scrapy.Spider):
 
     def parse(self, response):
         offers = response.css("div.search-list-item")
-        
-        for item in offers:
-        	best_price = item.css("div.price::text").extract_first().encode('ascii', 'replace').strip()[:-3]
-		    object = yield {
-		    	'productLinkId': self.link_dict[response.request.url],
-		    	'price': float(best_price),
-		    	'timestamp': datetime.datetime.now()
-		   	}
-		    url2 = 'http://localhost:8095/scrap'
-			response2 = requests.post(url2, data=object)
+
+	best_price = item.css("div.price::text").extract_first().encode('ascii', 'replace').strip()[:-3]
+	    object = yield {
+		'productLinkId': self.link_dict[response.request.url],
+		'price': float(best_price),
+		'timestamp': datetime.datetime.now()
+		}
+	    url2 = 'http://localhost:8095/scrap'
+		response2 = requests.post(url2, data=object)
 
 
